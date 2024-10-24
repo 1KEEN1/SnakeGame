cf7e2819462d97d8310b2b3acd44d836b6cb90bc
Ruslan Pirakov <rpirakov20@gmail.com>
2024-10-24 17:39:04 +0700

    Change compilation for the current files.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 g++ -c main.cpp
-g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system src/snake.cpp
+g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system src/snake.cpp src/fruit.cpp
 ./sfml-app
